d007e64c7d48d2b22d4b7064912efcab33311897
JaeHyun <wogus0518@naver.com>
2022-12-07 20:36:35 +0900

    UserControllerTest NPE 에러 해결

--- src/test/java/com/example/springsecurity1/controller/UserControllerTest.java
+++ src/test/java/com/example/springsecurity1/controller/UserControllerTest.java
@@ -3,23 +3,25 @@ package com.example.springsecurity1.controller;
 import com.example.springsecurity1.domain.UserRole;
 import com.example.springsecurity1.domain.dto.UserDto;
 import com.example.springsecurity1.domain.dto.UserJoinRequest;
+import com.example.springsecurity1.exception.ErrorCode;
+import com.example.springsecurity1.exception.UserException;
 import com.example.springsecurity1.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -28,6 +30,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
+
+    private final String loginId = "testId";
+    private final String password = "password";
+    private final String email = "test@gmail.com";
+    private final UserRole role = UserRole.USER;
 
     @Autowired
     MockMvc mockMvc;
@@ -43,15 +50,11 @@ class UserControllerTest {
     @WithMockUser
     @DisplayName("회원가입 성공")
     void join() throws Exception {
-        String loginId = "testId";
-        String password = "password";
-        String email = "test@gmail.com";
-        UserRole role = UserRole.USER;
 
         UserJoinRequest userJoinRequest = new UserJoinRequest(loginId, password, email, role);
         UserDto userDto = new UserDto(1L, loginId, password, email, role);
 
-        given(userService.join(userJoinRequest)).willReturn(userDto);
+        given(userService.join(any(UserJoinRequest.class))).willReturn(userDto);
 
         ResultActions result = mockMvc.perform(post("/api/v1/users/join")
                 .with(csrf())
@@ -63,9 +66,32 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.result.loginId").value(loginId))
                 .andExpect(jsonPath("$.result.password").doesNotExist())
                 .andExpect(jsonPath("$.result.email").value(email))
-                .andExpect(jsonPath("$.result.role").value(role))
+                .andExpect(jsonPath("$.result.role").value(role.name()))
                 .andDo(print());
 
         verify(userService).join(any(UserJoinRequest.class));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("회원가입 실패 - 아이디 중복")
+    void joinDuplicatedLoginId() throws Exception {
+
+        String errorMessage = "UserException{errorCode=DUPLICATED_LOGIN_ID, message='ID가 중복되었습니다.'}";
+
+        UserJoinRequest userJoinRequest = new UserJoinRequest(loginId, password, email, role);
+        UserException userException = new UserException(ErrorCode.DUPLICATED_LOGIN_ID);
+
+        when(userService.join(any(UserJoinRequest.class))).thenThrow(userException);
+
+        ResultActions result = mockMvc.perform(post("/api/v1/users/join")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userJoinRequest)));
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("ERROR"))
+                .andExpect(jsonPath("$.message").value(errorMessage))
+                .andDo(print());
     }
 }
